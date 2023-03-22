f410e8d547699c6cec89a7f9f4f070cf46cee871
Adam Slavny <adam.slavny@ufl.edu>
2023-03-21 21:42:20 -0400

    Scroll feature added
    Added a scroll for when looking at top and bottom

--- PDF Viewer/JS/script.js
+++ PDF Viewer/JS/script.js
@@ -1,14 +1,18 @@
-window.saveDataAcrossSessions = false
+window.saveDataAcrossSessions = true
 
 const LOOK_DELAY = 1000
+const PAGE_DELAY = 5000
 const TOP_CUTOFF = window.innerHeight / 4
 const BOTTOM_CUTOFF = window.innerHeight - window.innerHeight / 4
+const LEFT_CUTOFF = window.innerHeight / 4
+const RIGHT_CUTOFF = window.innerHeight - window.innerHeight / 4
 
 let startTime = Number.POSITIVE_INFINITY
 let lookDirection = null
 
 webgazer.setGazeListener((data, timestamp) => {
     if(data.y == null) return
+    if(data.x == null) return
 
     if(data.y < BOTTOM_CUTOFF && lookDirection !== 'BOTTOM') {
         startTime = timestamp
@@ -16,13 +20,30 @@ webgazer.setGazeListener((data, timestamp) => {
     } else if(data.y > TOP_CUTOFF && lookDirection !== 'TOP'){
         startTime = timestamp
         lookDirection = 'TOP'
-    } else if(data.y >= BOTTOM_CUTOFF && data.y <= TOP_CUTOFF){
+    // } else if(data.x < LEFT_CUTOFF && lookDirection !== 'LEFT') {
+    //     startTime = timestamp
+    //     lookDirection = 'LEFT'
+    // }else if(data.x > RIGHT_CUTOFF && lookDirection !== 'RIGHT'){
+    //     startTime = timestamp
+    //     lookDirection = 'RIGHT'
+    // }&& data.x >= LEFT_CUTOFF && data.x <= RIGHT_CUTOFF
+    }else if(data.y >= BOTTOM_CUTOFF && data.y <= TOP_CUTOFF){
         startTime = Number.POSITIVE_INFINITY
         lookDirection = null
     }
 
     if(startTime + LOOK_DELAY < timestamp) {
-        console.log("here")
+        if(lookDirection == 'TOP')
+        {
+            window.scrollBy(0,25);
+
+        }else if(lookDirection == 'BOTTOM') {
+            window.scrollBy(0,-25);
+        }else if(lookDirection == 'RIGHT'){
+            console.log("look right")
+        }else{
+            console.log("look left")
+        }
     }
 
 }).begin()
